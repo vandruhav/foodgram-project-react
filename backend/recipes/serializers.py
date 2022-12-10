563969b979b588bf9d2bc3db5594a97d12a64f14
Андрей Воробьёв <vandruhavv@yandex.ru>
2022-12-11 02:11:02 +0500

    flake8

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -3,8 +3,8 @@ import base64
 from django.core.files.base import ContentFile
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-
 from users.serializers import MyUserSerializer
+
 from .models import Ingredient, IngredientInRecipe, Recipe, Tag
 
 
