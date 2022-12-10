9a8c719f874b647f4ea0c82348bacc35e06c56f9
Андрей Воробьёв <vandruhavv@yandex.ru>
2022-12-11 01:36:44 +0500

    flake8

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.core.validators import RegexValidator, MinValueValidator
 from django.conf import settings
+from django.core.validators import MinValueValidator, RegexValidator
 
 
 class Tag(models.Model):
