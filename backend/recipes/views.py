af9a58c2d3d832bc7ed22b8c47effa3f491900c4
Андрей Воробьёв <vandruhavv@yandex.ru>
2022-12-11 02:05:19 +0500

    flake8

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -13,8 +13,8 @@ from rest_framework.response import Response
 from .filters import IngredientFilter, RecipeFilter
 from .models import Cart, Favorite, Ingredient, IngredientInRecipe, Recipe, Tag
 from .permissions import AuthorOrReadOnly
-from .serializers import IngredientSerializer, RecipeSerializer
-from .serializers import RecipesInFollowSerializer, TagSerializer
+from .serializers import (IngredientSerializer, RecipeSerializer,
+                          RecipesInFollowSerializer, TagSerializer)
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
