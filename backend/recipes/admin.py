a74cf8175544bf4a0ecf16068d4058d1bb7667d7
Андрей Воробьёв <vandruhavv@yandex.ru>
2022-12-11 02:02:55 +0500

    flake8

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Cart, Favorite, Ingredient, IngredientInRecipe, Recipe
-from .models import Tag, TagInRecipe
+from .models import (Cart, Favorite, Ingredient, IngredientInRecipe, Recipe,
+                     Tag, TagInRecipe)
 
 
 class TagAdmin(admin.ModelAdmin):
